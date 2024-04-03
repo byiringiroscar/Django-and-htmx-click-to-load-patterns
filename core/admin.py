3a9f42d23651e9eda0de0cadd801d335d5f248e8
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-03 13:33:13 +0200

    Add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Student
 
 # Register your models here.
+
+
+admin.site.register(Student)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    
+    def __str__(self):
+        return self.name

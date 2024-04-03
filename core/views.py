c6c3b0a92ebc52676a3b51bb43148febe5cbca1d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-03 13:42:02 +0200

    List the student

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from core.models import Student
 
 # Create your views here.
 def index(request):
     context = {}
+    return render(request, 'core/index.html', context)
+
+def students(request):
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
     return render(request, 'core/index.html', context)

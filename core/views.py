4ee5ebb0a8879833a555f74e0088c7143b7c1739
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-03 13:46:33 +0200

    Add pagination

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from core.models import Student
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -8,7 +9,10 @@ def index(request):
 
 def students(request):
     students = Student.objects.all()
+    paginator = Paginator(students, 2)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
     context = {
-        'students': students
+        'page_obj': page_obj
     }
     return render(request, 'core/index.html', context)
